f63c996b5e92c024c89a11db3e4a59d3611e882c
nureddinelmas <nureddinelmas34@gmail.com>
2022-11-14 07:54:26 +0100

    Initial Commit

---
+++ fast_coffee/fast_coffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fast_coffeeApp.swift
+//  fast_coffee
+//
+//  Created by Nureddin Elmas on 2022-11-14.
+//
+
+import SwiftUI
+
+@main
+struct fast_coffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
